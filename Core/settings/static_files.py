b128a9050e7d4ba40631c9ce03503bd9ace3d450
mikhail <mikhail@bashchkov.com>
2022-06-15 17:34:13 +0400

    Update

--- Core/settings/static_files.py
+++ Core/settings/static_files.py
@@ -1,7 +1,7 @@
 from Core.settings.common import BASE_DIR, DEBUG
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = BASE_DIR / 'static/'
+STATIC_ROOT = BASE_DIR / 'static/'
 
 # STATICFILES_DIRS = [
 #     STATIC_DIR
